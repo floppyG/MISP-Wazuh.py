38dbfa7a297f25c8de236c63dbbeed86476e732c
kermit <82391888+floppyG@users.noreply.github.com>
2024-06-27 16:46:49 +0200

    Update MISP_search.py

--- MISP_search.py
+++ MISP_search.py
@@ -1,5 +1,6 @@
 #!/var/ossec/framework/python/bin/python3
 # Copyright Whysecurity Srl Cellatica 2024.
+# TO TEST: ./CUTOM-MISP path/of/alert.json API_KEY
 
 import json
 import sys
@@ -22,35 +23,42 @@ socket_addr = "/var/ossec/queue/sockets/queue"  # Default Wazuh socket path
 
 # Configure logging
 logging.basicConfig(
-    filename='debug.log',
+    filename='/var/ossec/logs/debug.log',
     level=logging.DEBUG,
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%a %b %d %H:%M:%S %Z %Y'
 )
 
 def main(args):
+    """
+    Main function to execute the script.
+    """
     debug("Starting script execution")
-    
+
     # Check arguments
     if len(args) < 3:
         logging.error("Usage: python script_name.py path_to_alerts.json api_key")
         sys.exit(1)
 
-    # Getting input arguments by Wazuh 
+    # Getting input arguments by Wazuh
     alert_file_location = args[1]
     api_key = args[2]
 
     # Check arguments on CLI environment
     debug(f"File location: {alert_file_location}")
-    debug(f"API Key: {api_key}")
+    # debug(f"API Key: {api_key}")
 
     # Load alert. Parse JSON object.
     debug("Loading and parsing alert file")
-    with open(alert_file_location) as alert_file:
-        json_alert = json.load(alert_file)
-    
+    try:
+        with open(alert_file_location) as alert_file:
+            json_alert = json.load(alert_file)
+    except Exception as e:
+        logging.error(f"Failed to load or parse the alert file: {str(e)}")
+        sys.exit(1)
+
     debug("Alert loaded and parsed successfully")
-    debug(f"Parsed alert: {json.dumps(json_alert, indent=2)}")
+    debug(f"Parsed alert: {json_alert}")
 
     # Request MISP info
     debug("Requesting MISP info")
@@ -62,17 +70,25 @@ def main(args):
         process_misp_info(misp_info)
 
 def debug(msg):
+    """
+    Debug message logger.
+    """
     if debug_enabled:
         logging.debug(msg)
 
-# Check if the IP is public
 def is_public_ip(ip):
+    """
+    Check if the IP is public.
+    """
     ip_addr = ipaddress.ip_address(ip)
     return not ip_addr.is_private
 
 def query_misp(ip, ip_type, api_key):
+    """
+    Query the MISP server for information about the IP.
+    """
     debug(f"Querying MISP for IP: {ip}, type: {ip_type}")
-    url = 'https://your-MISP-ip/attributes/restSearch'
+    url = 'https://YOUR_MISP_IP/attributes/restSearch'
     headers = {
         'Accept': 'application/json',
         'Authorization': api_key,
@@ -83,32 +99,45 @@ def query_misp(ip, ip_type, api_key):
         'type': ip_type,
         'value': ip
     }
-    response = requests.post(url, headers=headers, json=payload, verify=False)
-    if response.status_code == 200:
-        debug(f"MISP query successful for IP: {ip}, type: {ip_type}")
-        return response.json()
-    else:
-        debug(f"Error: The MISP encountered an error. Status Code: {response.status_code}")
+    try:
+        response = requests.post(url, headers=headers, json=payload, verify=False)
+        if response.status_code == 200:
+            debug(f"MISP query successful for IP: {ip}, type: {ip_type}")
+            return response.json()
+        else:
+            debug(f"Error: The MISP encountered an error. Status Code: {response.status_code}")
+            sys.exit(1)
+    except Exception as e:
+        logging.error(f"Failed to query MISP: {str(e)}")
         sys.exit(1)
 
-# Check if the JSON is correctly formatted
 def request_misp_info(alert, api_key):
-    debug("Checking if alert contains 'data'")
-    if "data" not in alert:
-        debug("No data found in the alert.")
-        return None
-
+    """
+    Extract relevant IP information from the alert and query MISP.
+    """
+    debug("Checking if alert contains '_source -> data'")
     ip_info = {}
 
     debug("Extracting IP information from alert data")
-    if "srcip" in alert["data"]:
-        ip_info["srcip"] = alert["data"]["srcip"]
-
-    if "dstip" in alert["data"]:
-        ip_info["dstip"] = alert["data"]["dstip"]
-
-    if "srcPostNAT" in alert["data"]:
-        ip_info["srcPostNAT"] = alert["data"]["srcPostNAT"]
+    if "_source" in alert and "data" in alert["_source"]:
+        data = alert["_source"]["data"]
+        if "srcip" in data:
+            ip_info["srcip"] = data["srcip"]
+        if "dstip" in data:
+            ip_info["dstip"] = data["dstip"]
+        if "srcPostNAT" in data:
+            ip_info["srcPostNAT"] = data["srcPostNAT"]
+    elif "data" in alert:
+        data = alert["data"]
+        if "srcip" in data:
+            ip_info["srcip"] = data["srcip"]
+        if "dstip" in data:
+            ip_info["dstip"] = data["dstip"]
+        if "srcPostNAT" in data:
+            ip_info["srcPostNAT"] = data["srcPostNAT"]
+    else:
+        debug(f"No data found in JSON path")
+        return
 
     debug("Filtering out private IPs")
     ip_info = {key: value for key, value in ip_info.items() if is_public_ip(value)}
@@ -134,11 +163,18 @@ def request_misp_info(alert, api_key):
     return misp_data
 
 def process_misp_info(misp_info):
+    """
+    Process the MISP information and send relevant data to Wazuh.
+    """
     debug("Processing received MISP Info")
-    debug(f"MISP Info: {json.dumps(misp_info, indent=2)}")
-    print(misp_info)
-    send_event(misp_info)
 
+    for key, value in misp_info.items():
+        if isinstance(value, dict) and 'response' in value and 'Attribute' in value['response']:
+            attributes = value['response']['Attribute']
+
+            for attribute in attributes:
+                debug(f"MISP response each: {attribute}")
+                send_event(attribute)
 
 def send_event(msg, agent=None):
     """
@@ -162,7 +198,6 @@ def send_event(msg, agent=None):
         debug("Message sent to Wazuh socket")
     except Exception as e:
         debug(f"Failed to send message to Wazuh socket: {str(e)}")
-
 
 if __name__ == "__main__":
     try:
